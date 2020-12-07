4f0e86310d5c092a0b53f1b95297cbe0eb68e42c
kjkkuk <kjk.kuk@mail.ru>
2020-12-08 02:38:15 +0300

    new icons to sidebar, frontend properties fix

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
   # GET /properties
   # GET /properties.json
   def index
@@ -25,6 +25,7 @@ class PropertiesController < ApplicationController
   # POST /properties.json
   def create
     @property = Property.new(property_params)
+    @property.user_id = current_user.id
 
     respond_to do |format|
       if @property.save
