7e10eca4d2d493e5f4b57f02591743505f9a59b8
kjkkuk <kjk.kuk@mail.ru>
2020-12-09 04:58:17 +0300

    show sidebar for log in users, latest properties, special page for properties

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,7 @@
 module PropertiesHelper
+
+  def property_thumbnail property
+    img = property.photo.present? ? roperty.photo.thumb.url : ""
+    image_tag img, class: "property-thumb"
+  end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,6 @@
 class Property < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
+
+  scope :latest, -> {order created_at: :desc }
 end
