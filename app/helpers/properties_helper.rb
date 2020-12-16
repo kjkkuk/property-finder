f401f14c00a870d301a87bbc5d687db40c428553
kjkkuk <kjk.kuk@mail.ru>
2020-12-17 02:20:22 +0300

    add photos to properties

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,7 +1,12 @@
 module PropertiesHelper
 
   def property_thumbnail property
-    img = property.photo.present? ? roperty.photo.thumb.url : ""
+    img = property.photo.present? ? property.photo.thumb.url : ""
     image_tag img, class: "property-thumb"
+  end
+
+  def property_photo_url property
+    img = property.photo.present? ? property.photo.url : asset_url("")
+    image_tag img, class: "property-photo"
   end
 end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:                'AWS',                         # required
+    aws_access_key_id:       ENV['AWS_ACCESS_KEY_ID'],      # required unless using use_iam_profile
+    aws_secret_access_key:   ENV['AWS_SECRET_ACCESS_KEY'],  # required unless using use_iam_profile
+  }
+  config.storage = :fog
+  config.permissions = 0666
+  config.cache_dir = "#{Rails.root}/tmp/"
+  config.fog_directory  = ENV['S3_BUCKET']                                     # required
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
